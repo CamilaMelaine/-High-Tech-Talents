65a82e9b1c845d0fec00cf105427b75cfd389207
Camila Melaine <81929367+CamilaMelaine@users.noreply.github.com>
2022-03-17 01:01:43 -0300

    Envio do exercício

---
+++ Questão 8-1.py
@@ -0,0 +1,3 @@
+gh = int(input("Quanto voce ganha por hora: "))
+hm = int(input("Numero de horas trabalhadas no mes: "))
+print("Você ganha R$",(hm*gh))
---
+++ questão-10.py
@@ -0,0 +1,2 @@
+temperatura_c = float(input("digite a temperatura em Celsius:"))
+print("A temperatura em Fahrenheit é:",  round((temperatura_c*9/5) + 32, 2))
